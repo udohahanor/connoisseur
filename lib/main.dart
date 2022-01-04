88c7b26864271d7a071b8fc0286b10824a59ebf9
udohahanor <udohahanor@gmail.com>
2022-01-04 14:16:16 +0100

    FIrebase implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:connoisseur/screens/screens.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
